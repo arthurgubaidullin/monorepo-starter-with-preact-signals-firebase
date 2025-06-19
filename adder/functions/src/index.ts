49019fe39f26eca920088b8777984e84a462fbf3
Artur Gubaidullin <lapteuh@gmail.com>
2025-06-19 17:15:20 +0000

    Fix functions

--- adder/functions/src/index.ts
+++ adder/functions/src/index.ts
@@ -1,11 +1,11 @@
-import * as Adder from "@adder/business-rules";
 import { type AddRequest, type AddResponse } from "@adder/http-interfaces";
+import * as Factory from "@factory/in-server";
 import { onCall, type CallableRequest } from "firebase-functions/v2/https";
 
 const add = onCall((req: CallableRequest<AddRequest>): AddResponse => {
-  const result = Adder.add(req.data.a, req.data.b);
+  const adder = Factory.adder();
 
-  return { result: result };
+  return adder.add(req.data);
 });
 
 export const adder = { add };
