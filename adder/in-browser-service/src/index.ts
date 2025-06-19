af3d916b18e06614925d33fea4294949b79cc092
Artur Gubaidullin <lapteuh@gmail.com>
2025-06-19 15:24:13 +0000

    Add package

---
+++ adder/in-browser-service/src/index.ts
@@ -0,0 +1,31 @@
+import { signal, type Signal } from "@preact/signals";
+import {
+  httpsCallable,
+  type Functions,
+  type HttpsCallable,
+} from "firebase/functions";
+
+export class Adder {
+  #add: HttpsCallable<{ a: number; b: number }, { result: number }, unknown>;
+  #result: Signal<number | null> = signal(null);
+
+  constructor(functions: Functions) {
+    this.#add = httpsCallable(functions, "adder-add");
+  }
+
+  get result(): number | null {
+    return this.#result.value;
+  }
+
+  async add(this: this, a: number, b: number): Promise<void> {
+    this.#result.value = null;
+
+    try {
+      const response = await this.#add({ a, b });
+
+      this.#result.value = response.data.result;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
