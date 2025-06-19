f7ae7b3bd2aef1d874fabf10f8c59967cee81a73
Artur Gubaidullin <lapteuh@gmail.com>
2025-06-19 11:58:10 +0000

    Add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  { ignores: ["**/dist/**", "**/.turbo/**"] },
+  {
+    rules: {
+      "@typescript-eslint/method-signature-style": "error",
+      "react/react-in-jsx-scope": "off",
+      "no-restricted-imports": [
+        "warn",
+        {
+          patterns: [
+            {
+              regex: "^\\.\\./.+$",
+            },
+            {
+              regex: "^\\./.+/.+$",
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: "19",
+      },
+    },
+  },
+]);
