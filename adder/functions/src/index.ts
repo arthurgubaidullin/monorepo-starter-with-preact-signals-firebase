09913ac2d51c0b4b813b371231a8091285d05f65
Artur Gubaidullin <lapteuh@gmail.com>
2025-06-19 14:44:47 +0000

    Use local dep

--- adder/functions/src/index.ts
+++ adder/functions/src/index.ts
@@ -1,8 +1,12 @@
 import { logger } from "firebase-functions";
 import { onRequest } from "firebase-functions/v2/https";
+import { add } from "@adder/business-rules";
 
 export const helloWorld = onRequest((request, response) => {
-  logger.debug("Хочешь пирожок?");
+  const a = 2;
+  const b = 2;
+
+  logger.debug("Хочешь пирожок?", { a, b, result: add(a, b) });
 
   response.send("Hello from Firebase Function!").end();
 });
