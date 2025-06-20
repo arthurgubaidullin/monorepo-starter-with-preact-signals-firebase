00d6e22e5b69e8d2f74299693e6d35572fc49c28
Artur Gubaidullin <lapteuh@gmail.com>
2025-06-20 08:27:17 +0000

    Fix in-server factory

--- factory/in-server/src/factory.ts
+++ factory/in-server/src/factory.ts
@@ -2,11 +2,17 @@ import { Adder } from "@adder/in-server-service";
 import { logger } from "firebase-functions";
 
 export class Factory {
+  #adder: Adder | null = null;
+
   get logger(): typeof logger {
     return logger;
   }
 
   get adder(): Adder {
-    return new Adder(this);
+    if (!this.#adder) {
+      this.#adder = new Adder(this);
+    }
+
+    return this.#adder;
   }
 }
